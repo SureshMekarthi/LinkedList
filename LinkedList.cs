e43f33d1b82cf41762335013f87494c8f91176ee
AnushaPoosala <anusha.poosala@outlook.com>
2024-07-01 23:06:03 -0400

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public LinkedList()
+        {
+            this.Head = null;
+        }
+        public void Add(int data)
+        {
+            Node newNode= new Node(data);
+            if(Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while(current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void Remove(int data)
+        {
+            if (Head == null)
+            {
+                return;
+            }
+            Head = Head.Next;
+        }
+        public void PrintList()
+        {
+            Node current = Head;
+            while (current !=null)
+                {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 12, 5, 8, 7 };
+            LinkedList head = DeleteHead(head);
+        }
+        private static Node DeleteHead(Node head)
+        {
+            if(head == null)
+            {
+                return head;
+            }
+            head = head.Next;
+            return head;
+        }
+
+    }
+}
